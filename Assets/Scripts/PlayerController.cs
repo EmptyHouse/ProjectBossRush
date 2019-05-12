bc2cec043485a4780fe52145953c9dcffe1b43d9
Ryan Andersen <ryan.andersen05@gmail.com>
2019-05-12 13:14:38 -0400

    Making changes to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     #region const inputs
     public const string PAUSE_GAME_INPUT = "Pause";
-
-    public string HORIZONTAL_INPUT_ANIMATOR_PARENTER = "HorizontalInput";
-    public const string VERTICAL_INPUT_ANIMATOR_PARAMETER = "VerticalInput";
+    public const string HORIZONTAL_AXIS = "Horizontal";
+    public const string VERTICAL_AXIS = "Vertical";
 
     public const string MELEE_INPUT = "Melee";
     public const string PROJECTILE_INPUT = "FireProjectile";
