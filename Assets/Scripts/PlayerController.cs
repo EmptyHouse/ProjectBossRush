91b33fb10fd4d3aa71f05ffdefe074d11380aa6b
Ryan Andersen <ryan.andersen05@gmail.com>
2019-05-30 23:20:13 -0400

    Movement is now based on the direction that the camera is looking to feel more natural

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,7 +24,21 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
-        characterStats.characterMovement.SetMovementInput(Input.GetAxisRaw(HORIZONTAL_AXIS), Input.GetAxisRaw(VERTICAL_AXIS));
+        Vector2 adjustedPlayerInput = AdjustPlayerInputBasedOnCamera(new Vector2(Input.GetAxisRaw(HORIZONTAL_AXIS), Input.GetAxisRaw(VERTICAL_AXIS)));
+        characterStats.characterMovement.SetMovementInput(adjustedPlayerInput.x, adjustedPlayerInput.y);
+    }
+
+    /// <summary>
+    /// Method to adjust the player's input based on the direction of the camera
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 AdjustPlayerInputBasedOnCamera(Vector2 originalPlayerInput)
+    {
+        Camera cam = GameOverseer.Instance.mainGameCamera;
+        Vector3 playerInput3 = new Vector3(originalPlayerInput.x, 0, originalPlayerInput.y);
+        Vector3 updatedInput = cam.transform.TransformDirection(playerInput3);
+        return new Vector2(updatedInput.x, updatedInput.z);
+
     }
     #endregion monobehaviour methodss
 
