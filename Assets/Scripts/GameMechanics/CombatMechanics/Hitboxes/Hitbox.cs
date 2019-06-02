3d6018b81a883454aa4dd2c89b1e1fbb7a12cc05
Ryan Andersen <ryan.andersen05@gmail.com>
2019-06-02 19:35:09 -0400

    hitbox events added

--- Assets/Scripts/GameMechanics/CombatMechanics/Hitboxes/Hitbox.cs
+++ Assets/Scripts/GameMechanics/CombatMechanics/Hitboxes/Hitbox.cs
@@ -1,8 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+
+[RequireComponent(typeof(Collider))]
 public class Hitbox : MonoBehaviour
 {
+    private CharacterStats associatedCharacterStats;
+    #region hitbox event variables
+    private UnityEvent OnHitboxEnteredEvent;
+    private UnityEvent OnHitboxExitEvent;
     
+    private UnityEvent OnHurtboxEnteredEvent;
+    private UnityEvent OnHurtboxExitEvent;
+
+    private UnityEvent OnOtherColliderEnter;
+    private UnityEvent OnOtherColliderExit;
+    #endregion hitbox event variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        associatedCharacterStats = GetComponentInParent<CharacterStats>();
+        if (!associatedCharacterStats)
+        {
+            Debug.LogWarning("There were no character stats for this hitbox");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Hurtbox hurtbox = other.GetComponent<Hurtbox>();
+        Hitbox hitbox = other.GetComponent<Hitbox>();
+        if (hurtbox)
+        {
+
+        }
+        if (hitbox)
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Hurtbox hurtbox = other.GetComponent<Hurtbox>();
+        Hitbox hitbox = other.GetComponent<Hitbox>();
+
+        if (hurtbox)
+        {
+
+        }
+        if (hitbox)
+        {
+
+        }
+    }
+    #endregion monobehaviour methods
 }
