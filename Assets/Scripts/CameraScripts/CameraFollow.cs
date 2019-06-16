9178fc297a1ecc21af64c06910ce1178a06ee3e4
Ryan Andersen <ryan.andersen05@gmail.com>
2019-06-16 15:30:05 -0400

    Adding camera zoom into our game

--- Assets/Scripts/CameraScripts/CameraFollow.cs
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -4,16 +4,98 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform targetTransformObject;
+    #region main variables
+    public AnimationCurve zoomingAnimationCurve;
+    [Tooltip("The lerp speed of our camera from the target transform")]
+    public float cameraFollowSpeed = 10;
+    /// <summary>
+    /// This value is used for the default offset of our camera from the player
+    /// </summary>
+    private Vector3 defaultOffset;
+    /// <summary>
+    /// If our camera is currently in the process of zooming in or out this variable will be set to true.
+    /// </summary>
+    private bool isZooming;
+    private Camera associatedCamera;
+    #endregion main variables
+
+    public Transform targetTransformObject { get; set; }
+    
     #region monbehaviour methods
+
     private void Awake()
     {
+        targetTransformObject = GetComponentInParent<PlayerCharacterStats>().transform;
+        defaultOffset = this.transform.localPosition;
+        this.transform.SetParent(null);
+        associatedCamera = GetComponent<Camera>();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            SetCameraSize(associatedCamera.orthographicSize + 5, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            SetCameraSize(associatedCamera.orthographicSize - 5, 1);
+        }
     }
 
     private void LateUpdate()
     {
-
+        LerpToTargetTransform();
     }
     #endregion monobehaviour methods
+
+    /// <summary>
+    /// Lerps the camera to the target position
+    /// 
+    /// </summary>
+    private void LerpToTargetTransform()
+    {
+        Vector3 goalPosition = targetTransformObject.position + defaultOffset;
+
+        print(goalPosition);
+        if (targetTransformObject)
+        {
+            transform.position = Vector3.Lerp(transform.position, goalPosition, Time.deltaTime * cameraFollowSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Sets the camera scale. You can zoom in and out with this method
+    /// </summary>
+    public void SetCameraSize(float sizeToSet, float timeToReachGoalSize = .25f)
+    {
+        StartCoroutine(SetCameraToDesiredSize(sizeToSet, timeToReachGoalSize));
+    }
+
+    /// <summary>
+    /// Coroutine that sets the camera to the desired size gradually
+    /// </summary>
+    /// <param name="sizeToSet"></param>
+    /// <param name="timeToReachGoalSize"></param>
+    /// <returns></returns>
+    private IEnumerator SetCameraToDesiredSize(float sizeToSet, float timeToReachGoalSize)
+    {
+        isZooming = false;
+        yield return null;
+        float originalSize = associatedCamera.orthographicSize;
+
+        isZooming = true;
+        float timeThatHasPassed = 0;
+        while(timeThatHasPassed < timeToReachGoalSize)
+        {
+            if (!isZooming)
+            {
+                yield break;
+            }
+            associatedCamera.orthographicSize = Mathf.Lerp(originalSize, sizeToSet, zoomingAnimationCurve.Evaluate(timeThatHasPassed / timeToReachGoalSize));
+            yield return null;
+            timeThatHasPassed += Time.deltaTime;
+        }
+        associatedCamera.orthographicSize = sizeToSet;
+    }
 }
