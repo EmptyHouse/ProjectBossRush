97b4df5b6c12b88171d9c673e89bd28cb5fcce38
Ryan Andersen <ryan.andersen05@gmail.com>
2019-06-15 22:49:30 -0400

    updates to hitbox and adding hurtboxes

--- Assets/Scripts/GameMecahnics/HitboxMechanics/Hitbox.cs
+++ Assets/Scripts/GameMecahnics/HitboxMechanics/Hitbox.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 
 public abstract class Hitbox : MonoBehaviour
 {
-    
+    private UnityEvent<Collider2D> OnHitboxEnterColliderEvent;
+    private UnityEvent<Collider2D> OnHitboxExitColliderEvent;
+    private UnityEvent<Hurtbox> OnHitboxEnterHurtboxEvent;
+    private UnityEvent<Hurtbox> OnHitboxExitHurtboxEvent;
+
+    public void AddActionToHitboxEnterHurtboxEvent()
+    {
+
+    }
+
 }
