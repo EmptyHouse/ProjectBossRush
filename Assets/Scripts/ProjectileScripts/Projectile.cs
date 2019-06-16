5ea8f8b39bbd994980216547a68f87b34739d47f
Ryan Andersen <ryan.andersen05@gmail.com>
2019-06-15 22:17:39 -0400

    Adding hitbox scripts

--- Assets/Scripts/ProjectileScripts/Projectile.cs
+++ Assets/Scripts/ProjectileScripts/Projectile.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    
     /// <summary>
     /// A reference to the character that fired this projectile
     /// </summary>
     public CharacterStats associatedCharacterStats { get; set; }
+    /// <summary>
+    /// Associated rigid body with our projectile
+    /// </summary>
+    private Rigidbody2D rigid;
+
+
+    #region monobehaviour methods
+    protected virtual void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+    #endregion monobehaviour methods
 
 }
