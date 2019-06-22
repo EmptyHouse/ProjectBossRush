900dbafe5d84c320716d4b273efca11dbb28530c
Ryan Andersen <ryan.andersen05@gmail.com>
2019-06-22 17:06:41 -0400

    Created a basic menu manager for our game
    Should help with keeping things consistent among menus in our game

--- Assets/Scripts/UIScripts/SelectableMenuManager.cs
+++ Assets/Scripts/UIScripts/SelectableMenuManager.cs
@@ -9,20 +9,158 @@ public class SelectableMenuManager : MonoBehaviour
 {
     #region const variables
     public const float JOYSTICK_THRESHOLD = .6f;
-    public const float HORIZONTAL_INPUT = "Horizontal";
-    public const float VERTICAL_AXIS = "Vertical";
+    public const string HORIZONTAL_AXIS = "Horizontal";
+    public const string VERTICAL_AXIS = "Vertical";
+
+    public const float DELAY_BEFORE_BEGIN_AUTO_SCROLL = .12f;
+    public const float DELAY_BETWEEN_NEXT_OPTION_AUTO_SCROLL = .03f;
     #endregion const variables
 
     public SelectableOption initialOptionToSelect;
 
+    private SelectableOption currentlySelectedOption;
+    private bool isAutoScrolling;
     #region monobehaviour methods
    
     private void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw(HORIZONTAL_AXIS);
+        float verticalInput = Input.GetAxisRaw(VERTICAL_AXIS);
+
+        if (isAutoScrolling) return;
+
+        if (Mathf.Abs(horizontalInput) > JOYSTICK_THRESHOLD)
+        {
+            StartCoroutine(AutoScrollHorizontal(horizontalInput));
+        }
+        else if (Mathf.Abs(verticalInput) > JOYSTICK_THRESHOLD)
+        {
+            StartCoroutine(AutoScrollVertical(verticalInput));
+        }
     }
     #endregion monobehaviour methods
 
+    #region controls
+    protected float GetHorizontalInput()
+    {
+        return Input.GetAxisRaw(HORIZONTAL_AXIS);
+    }
 
+    protected float GetVerticalInput()
+    {
+        return Input.GetAxisRaw(VERTICAL_AXIS);
+    }
+    #endregion controls
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="hInput"></param>
+    /// <param name="vInput"></param>
+    protected void SetNextOption(int hInput, int vInput)
+    {
+        if (hInput > 0)
+        {
+            SelectNextOption(currentlySelectedOption.GetOptionEast());
+        }
+        else if (hInput < 0)
+        {
+            SelectNextOption(currentlySelectedOption.GetOptionWest());
+        }
+
+
+        if (vInput > 0)
+        {
+            SelectNextOption(currentlySelectedOption.GetOptionNorth());
+        }
+        else if (vInput < 0)
+        {
+            SelectNextOption(currentlySelectedOption.GetOptionSouth());
+        }
+    }
+
+    private void SelectNextOption(SelectableOption optionToSelect)
+    {
+        if (optionToSelect == null)
+        {
+            return;
+        }
+
+        if (currentlySelectedOption != null)
+        {
+            currentlySelectedOption.enabled = false;
+        }
+
+        currentlySelectedOption = optionToSelect;
+        currentlySelectedOption.enabled = true;
+    }
+
+    private IEnumerator AutoScrollHorizontal(float horizontalInput)
+    {
+        isAutoScrolling = true;
+        int direction = (int)Mathf.Sign(horizontalInput);
+        float timeThatHasPassed = 0;
+        SetNextOption(direction, 0);
+        while (timeThatHasPassed < DELAY_BEFORE_BEGIN_AUTO_SCROLL)
+        {
+            if (direction * GetHorizontalInput() < JOYSTICK_THRESHOLD)
+            {
+                isAutoScrolling = false;
+                yield break;
+            }
+
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        SetNextOption(direction, 0);
+        timeThatHasPassed = 0;
+        while (direction * GetHorizontalInput() > JOYSTICK_THRESHOLD)
+        {
+            if (timeThatHasPassed > DELAY_BETWEEN_NEXT_OPTION_AUTO_SCROLL)
+            {
+                timeThatHasPassed = 0;
+                SetNextOption(direction, 0);
+            }
+
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        isAutoScrolling = false;
+    }
+
+    private IEnumerator AutoScrollVertical(float verticalInput)
+    {
+        isAutoScrolling = true;
+        int direction = (int)Mathf.Sign(verticalInput);
+        float timeThatHasPassed = 0;
+        SetNextOption(0, direction);
+        while (timeThatHasPassed < DELAY_BEFORE_BEGIN_AUTO_SCROLL)
+        {
+            if (direction * GetVerticalInput() < JOYSTICK_THRESHOLD)
+            {
+                isAutoScrolling = false;
+                yield break;
+            }
+
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        SetNextOption(0, direction);
+        timeThatHasPassed = 0;
+        while (direction * GetVerticalInput() > JOYSTICK_THRESHOLD)
+        {
+            if (timeThatHasPassed > DELAY_BETWEEN_NEXT_OPTION_AUTO_SCROLL)
+            {
+                timeThatHasPassed = 0;
+                SetNextOption(0, direction);
+            }
+
+            timeThatHasPassed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        isAutoScrolling = false;
+    }
 
 }
--- Assets/Scripts/UIScripts/UIElementScripts/SelectableOption.cs
+++ Assets/Scripts/UIScripts/UIElementScripts/SelectableOption.cs
@@ -4,9 +4,108 @@ using UnityEngine;
 
 public class SelectableOption : MonoBehaviour
 {
-    public SelectableOption northOption;
-    public SelectableOption southOption;
-    public SelectableOption eastOption;
-    public SelectableOption westOption;
+    [SerializeField]
+    private SelectableOption northOption;
+    [SerializeField]
+    private SelectableOption southOption;
+    [SerializeField]
+    private SelectableOption eastOption;
+    [SerializeField]
+    private SelectableOption westOption;
 
+    public bool optionActive { get; private set; }
+    private bool wasInspected;
+
+    public void SetOptionActive(bool optionActive)
+    {
+
+    }
+
+
+    public SelectableOption GetOptionNorth()
+    {
+        if (northOption == null || wasInspected)
+        {
+            return null;
+        }
+
+        if (optionActive)
+        {
+            return northOption;
+        }
+        SelectableOption optionToReturn = null;
+        if (northOption != null)
+        {
+            wasInspected = true;
+            optionToReturn = northOption.GetOptionNorth();
+            wasInspected = false;
+        }
+        return optionToReturn;
+        
+    }
+
+    public SelectableOption GetOptionSouth()
+    {
+        if (southOption == null || wasInspected)
+        {
+            return southOption;
+        }
+
+        if (optionActive)
+        {
+            return southOption;
+        }
+        SelectableOption optionToReturn = null;
+        if (southOption != null)
+        {
+            wasInspected = true;
+            optionToReturn = southOption.GetOptionSouth();
+            wasInspected = false;
+        }
+        return optionToReturn;
+    }
+
+    public SelectableOption GetOptionEast()
+    {
+        if (eastOption == null || wasInspected)
+        {
+            return null;
+        }
+
+        if (optionActive)
+        {
+            return eastOption;
+        }
+        SelectableOption optionToReturn = null;
+        if (eastOption != null)
+        {
+            wasInspected = true;
+            optionToReturn = eastOption.GetOptionEast();
+            wasInspected = false;
+        }
+        return optionToReturn;
+    }
+
+    public SelectableOption GetOptionWest()
+    {
+        if (westOption == null || wasInspected)
+        {
+            return null;
+        }
+
+        if (optionActive)
+        {
+            return westOption;
+        }
+        SelectableOption optionToReturn = null;
+        if (westOption != null)
+        {
+            wasInspected = true;
+            optionToReturn = westOption.GetOptionWest();
+            wasInspected = false;
+        }
+        return optionToReturn;
+    }
+
+    
 }
