5644e4f0907ca8b6976fe898776f4c308f3d219e
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2019-07-07 15:59:36 -0400

    Added projectile prefab

--- Assets/Scripts/ProjectileScripts/Projectile.cs
+++ Assets/Scripts/ProjectileScripts/Projectile.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    
+
+    #region main variables
+
+    private const float Offset = 10f;
+
     /// <summary>
     /// A reference to the character that fired this projectile
     /// </summary>
-    public CharacterStats associatedCharacterStats { get; set; }
+    public CharacterStats AssociatedCharacterStats { get; set; }
+
     /// <summary>
     /// Associated rigid body with our projectile
     /// </summary>
     private Rigidbody2D rigid;
+
+    private Animator Animator { get; set; }
+
+    /// <summary>
+    /// Direction of the projectile. Usually shouldn't change once projectile is instantiated.
+    /// Can maybe change if projectile arcs, but that's a different story.
+    /// </summary>
+    private Vector2 Direction;
+
+    #endregion
 
 
     #region monobehaviour methods
     protected virtual void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     protected virtual void Update()
@@ -26,5 +42,14 @@ public class Projectile : MonoBehaviour
         
     }
     #endregion monobehaviour methods
+
+    #region public methods
+
+    public void SetupProjectile(Vector2 Direction, CharacterStats associatedCharacterInfo)
+    {
+        AssociatedCharacterStats = associatedCharacterInfo;
+    }
+
+    #endregion
 
 }
